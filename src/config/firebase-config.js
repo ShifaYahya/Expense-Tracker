3ed066dd1cbc391344de07225fdb033959d49586
Shifa <shifayahya327@gmail.com>
2024-06-26 01:17:14 +0530

    added something

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -22,7 +22,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+const analytics = getAnalytics(app);
 //we write by ouer own
 export const auth= getAuth(app)
 export const provider = new GoogleAuthProvider()
